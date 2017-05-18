59320a3ccfd9880b93522ed0ec7de0ec8b6e732a
sun <asmh1989@gmail.com>
2017-05-18 13:28:20 +0800

    优化android ui模版生成

--- templates/ui_android.js
+++ templates/ui_android.js
@@ -82,7 +82,7 @@ public class ${name} extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-
+        super.onLayout(changed, l, t, r, b);
     }
     
     ${propsFunc}
