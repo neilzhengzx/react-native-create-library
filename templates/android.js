eb06a1045447e29210a5f915394cf711914ff50d
sun <asmh1989@gmail.com>
2017-08-02 13:01:49 +0800

    适配 0.47

--- templates/android.js
+++ templates/android.js
@@ -136,11 +136,6 @@ public class ${name}Package implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Collections.emptyList();
     }
--- templates/ui_android.js
+++ templates/ui_android.js
@@ -265,11 +265,6 @@ public class ${name}Package implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
       return Arrays.<ViewManager>asList(
         ${ColllectionViews}
